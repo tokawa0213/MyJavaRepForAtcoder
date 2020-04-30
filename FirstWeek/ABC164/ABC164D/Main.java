27cccaaf6e90d66ccfe46200fec01f8d63986af6
tokawa0213 <tokawa0213@gmail.com>
2020-04-30 19:52:46 +0900

    problem in D

--- FirstWeek/ABC164/ABC164D/Main.java
+++ FirstWeek/ABC164/ABC164D/Main.java
@@ -1,5 +1,35 @@
-package FirstWeek.ABC164.ABC164D;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> remainder_mapping = new HashMap<>();
+        for(int i = 0;i < 2019;i++){
+            remainder_mapping.put(i,0);
+        }
+        Scanner sc = new Scanner(System.in);
+        String target_string = sc.next();
 
+        for(int i = 0;i < target_string.length(); i++){
+            // 019と19が同じ扱いになってしまうので。。。
+            if(!target_string.substring(i,target_string.length()).substring(0,1).equals("0")){
+                BigInteger target = new BigInteger(target_string.substring(i,target_string.length()));
+                remainder_mapping.put(target.remainder(BigInteger.valueOf(2019)).intValue(),remainder_mapping.get((target.remainder(BigInteger.valueOf(2019))).intValue())+1);
+            }
+        }
+        int answer = 0;
+        for(int i = 0;i < 2019;i++){
+            int candidates = remainder_mapping.get(i);
+            if(candidates != 0){
+                answer += ((candidates-1)*(candidates))/2;
+                if(i == 0){
+                    answer += candidates;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
 }
